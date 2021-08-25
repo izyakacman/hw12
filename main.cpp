420561097af4cacf5abbbb21feb09c42875fa469
Matlyuk Ilia <Ilia.Matlyuk@infotecs.ru>
2021-08-25 09:46:04 +0300

    delete debug

--- main.cpp
+++ main.cpp
@@ -12,9 +12,6 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	// const char* model = "saved_model";
-	// const char* test_file = "test.csv";
-
 	std::cout << "Calculations in progress, please wait...\n";
 
 	std::cout << GetTensorAccuracy(argv[2], argv[1]) << endl;
